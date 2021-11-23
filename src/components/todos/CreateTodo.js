8042d85db9e72f9e5005dee1ae29f42e250c17d5
Jenny Kim <kimj1905@gmail.com>
2021-11-22 19:14:32 -0500

    commit tree 48212eeb90928e0533b258829e709f69138271a8

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,49 +1,46 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CreateTodo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-    };
-  }
+	state = {
+		text: ""
+	};
 
-  handleChange = event => {
-    this.setState({
-      text: event.target.value
-    });
-  };
+	handleChange = (event) => {
+		this.setState({
+			text: event.target.value
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-    this.setState({
-      text: '',
-    })
-  }
-  
-  render() {
-    return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="add todo" 
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <input type="submit" />
-        </form>
-      </div>
-    )
-  }
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.props.addTodo(this.state);
+		this.setState({
+			text: ""
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="add todo"
+						onChange={this.handleChange}
+						value={this.state.text}
+					/>
+					<input type="submit" />
+				</form>
+			</div>
+		);
+	}
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
-  }
-}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addTodo: (formData) => dispatch({ type: "ADD_TODO", payload: formData })
+	};
+};
 
 export default connect(null, mapDispatchToProps)(CreateTodo);
--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Todo = props => {
-  return (
-    <li>{props.text}</li>
-  );
+const Todo = (props) => {
+	console.log(props);
+	return (
+		<li key={props.todo.id}>
+			<span>{props.todo.text}</span>
+			<button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+		</li>
+	);
 };
 
 export default Todo;
